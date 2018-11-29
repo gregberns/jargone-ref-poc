ae7ffcf6a5ef2f6e8e92f165e71b118960686fa1
Greg Berns <gregberns@gmail.com>
2018-11-28 22:02:19 -0700

    port

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const url = require('url');
 const helper = require('./helpers')
 var path = require('path')
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 
